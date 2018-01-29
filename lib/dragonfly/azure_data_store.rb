e027c4d0c7bdd8e0fb3d75d8647112f19c160972
Alter Lagos <alagos@users.noreply.github.com>
2018-01-29 16:18:57 -0300

    Basic structure

--- lib/dragonfly/azure_data_store.rb
+++ lib/dragonfly/azure_data_store.rb
@@ -1,7 +1,23 @@
-require "dragonfly/azure_data_store/version"
+require 'dragonfly/azure_data_store/version'
+
+Dragonfly::App.register_datastore(:azure) { Dragonfly::AzureDataStore }
 
 module Dragonfly
-  module AzureDataStore
-    # Your code goes here...
+  class AzureDataStore
+    attr_accessor :account_name, :access_key
+
+    def initialize(opts = {})
+      @account_name = opts[:account_name]
+      @access_key = opts[:access_key]
+    end
+
+    def write(content, opts = {})
+    end
+
+    def read(uid)
+    end
+
+    def destroy(uid)
+    end
   end
 end
